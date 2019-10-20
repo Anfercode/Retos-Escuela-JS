2bf9924594bc2a3de821b3c7ca6893230ca26c3e
Andres Campero <andres.campero@frubana.com>
2019-10-20 13:30:33 -0500

    Solution of the 4 challenge

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
-# escuelajs-reto-03
-Reto 3 Septiembre 14: Curso de Fundamentos de JavaScript
+# escuelajs-reto-04
+Reto 4 Septiembre 21: Curso de Profesional de JavaScript
 
-# Instalación
-
-```
-npm install
-```
 # Ejecución
 
 ```
@@ -13,18 +8,45 @@ npm run start
 ```
 
 ### Primer problema
-La aplicación tiene errores que no permiten ejecutarla, lee detenidamente el código y determina dónde se encuentran al ejecutarlo en la consola.
+Crea una función llamada "randomTime" que te permita retornar un valor en mili segundos de forma aleatoria en el rango de 1000ms hasta 8000ms.
+
+* Completa la función "orders" manejando el reject.
+* Utiliza la función de randomTime
 
 ### Segundo Problema
-Una vez que tu aplicación ya esté funcionando convierte el código a ECMAScript 6 (ES6)
-* Arrow Functions
-* Template Strings
+
+Crea una función llamada "waiter2" que se encargue de recoger dos pedidos, uno de la "Mesa 1" y otro de la "Mesa 3".
+
+Pedido "Mesa 1": Combo Hotdog
+Pedido "Mesa 3": Combo Pizza
+
+* Utiliza Promesas Encadenadas
+* Utiliza la función de randomTime
 
 ### Tercer Problema
-Transforma el código escrito en ECMAScript6(ES6) para que funcione con promesas y así evitar el Callback Hell del final.
 
-# Contributing
-If someone wants to add or improve something, I invite you to collaborate directly in this repository: [escuelajs-reto-03](https://github.com/platzi/escuelajs-reto-03/)
+Crea una función llamada "waiter3" que se encargue de recoger el pedido de la "Mesa 2" el pedido solo puede ser entregado hasta que todos los plantillos estén listos para ser servidos.
 
-# License
-escuelajs-reto-03 is released under the [MIT License](https://opensource.org/licenses/MIT).
+* Pedido "Mesa 2": Combo Hotdog, Combo Pizza, Combo Hotdog
+
+* Utiliza Async/Await
+* Manejo de errores
+* Utiliza la función de randomTime
+
+### Cuarto Problema (Opcional)
+
+Crea una función llamada "fetchOrders" que realice un llamado a la API de ordenes y una función llamada "waiter4" que se encargue de solicitar 4 pedidos que deban de ser entregados hasta que estén todos listos.
+
+* API: https://us-central1-escuelajs-api.cloudfunctions.net/orders
+* Utiliza Async/Await
+* Manejo de errores
+
+# Enviar solución de reto
+
+Debes de crear un "Fork" de este proyecto, revolver los problemas y crear un Pull Request hacia este repositorio.
+
+# Contribuir
+Si alguien quiere agregar o mejorar algo, lo invito a colaborar directamente en este repositorio: [escuelajs-reto-04](https://github.com/platzi/escuelajs-reto-04/)
+
+# Licencia
+escuelajs-reto-04 se lanza bajo la licencia [MIT](https://opensource.org/licenses/MIT).
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = "https://rickandmortyapi.com/api/character/";
+var API = "https://us-central1-escuelajs-api.cloudfunctions.net/orders";
 var xhttp = new XMLHttpRequest();
 
-const fetchDataPromise = url_api => {
+const fetchOrders = url_api => {
   return new Promise((resolve, reject) => {
     xhttp.onreadystatechange = function(event) {
       if (xhttp.readyState === 4 && xhttp.status == 200)
@@ -15,28 +15,117 @@ const fetchDataPromise = url_api => {
   });
 };
 
-fetchDataPromise(API)
-  .then(
-    (firstCall = data1 => {
-      console.log("Primer Llamado...");
-      parcedData1 = JSON.parse(data1);
-      return fetchDataPromise(API + parcedData1.results[0].id);
+function randomTime() {
+  MIN = 1;
+  MAX = 8;
+  return Math.floor(Math.random() * (MAX - MIN + 1) + MIN) * 1000;
+}
+
+function randomTable() {
+  MIN = 0;
+  MAX = 3;
+  return Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
+}
+
+onError = error => {
+  console.log(error);
+};
+
+const orders = (time, product, table) => {
+  console.log(`### Orden: ${product} para ${table}`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
+      );
+    }, time);
+  });
+};
+
+const menu = {
+  hamburger: "Combo Hamburguesa",
+  hotdog: "Combo Hot Dogs",
+  pizza: "Combo Pizza"
+};
+
+const table = ["Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4", "Mesa 5"];
+
+const waiter = () => {
+  orders(randomTime(), menu.hamburger, table[3])
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+};
+
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0])
+    .then(res => {
+      console.log(res);
+      return orders(randomTime(), menu.pizza, table[2]);
     })
-  )
-  .then(
-    (secondCall = data2 => {
-      console.log("Segundo Llamado...");
-      parcedData2 = JSON.parse(data2);
-      return fetchDataPromise(parcedData2.origin.url);
-    })
-  )
-  .then(
-    (thirdCall = data3 => {
-      parcedData3 = JSON.parse(data3);
-      console.log("Tercero Llamado...");
-      console.log("Personajes:" + " " + parcedData1.info.count);
-      console.log("Primer Personaje:" + " " + parcedData2.name);
-      console.log("Dimensión:" + " " + parcedData3.dimension);
-    })
-  )
-  .catch();
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+};
+
+async function waiter3() {
+  const random = randomTime();
+
+  try {
+    await Promise.all([
+      orders(random, menu.hotdog, table[1]).then(res => console.log(res)),
+      orders(random, menu.pizza, table[1]).then(res => console.log(res)),
+      orders(random, menu.hamburger, table[1]).then(res => console.log(res))
+    ]);
+  } catch (error) {
+    onError(error);
+  }
+}
+
+async function waiter4() {
+  const time = randomTime();
+  try {
+    const promises = await Promise.all([
+      fetchOrders(API).then(
+        (callOrder = menu => {
+          return (parcedData = JSON.parse(menu).data);
+        })
+      ),
+      fetchOrders(API).then(
+        (callOrder = menu => {
+          return (parcedData = JSON.parse(menu).data);
+        })
+      ),
+      fetchOrders(API).then(
+        (callOrder = menu => {
+          return (parcedData = JSON.parse(menu).data);
+        })
+      ),
+      fetchOrders(API).then(
+        (callOrder = menu => {
+          return (parcedData = JSON.parse(menu).data);
+        })
+      )
+    ]);
+
+    await Promise.all([
+      orders(time, promises[0], table[randomTable()]).then(res =>
+        console.log(res)
+      ),
+      orders(time, promises[1], table[randomTable()]).then(res =>
+        console.log(res)
+      ),
+      orders(time, promises[2], table[randomTable()]).then(res =>
+        console.log(res)
+      ),
+      orders(time, promises[3], table[randomTable()]).then(res =>
+        console.log(res)
+      )
+    ]);
+  } catch (error) {
+    onError(error);
+  }
+}
+
+waiter();
+waiter2();
+waiter3();
+waiter4();
